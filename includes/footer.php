fd8bfd087a87e7f008d275a8742218652a1f71fc
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2025-12-25 11:16:10 +0100

    feat: création du pied de page avec liens utiles et crédits CAN 2025

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<footer class="bg-dark text-white pt-5 pb-3 mt-5">
+    <div class="container text-center text-md-start">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <h5>ASSAD ZOO</h5>
+                <p>Vivez l'expérience sauvage au cœur du Maroc pendant la CAN 2025. Un zoo virtuel éducatif pour toute l'Afrique.</p>
+            </div>
+            <div class="col-md-4 mb-4 text-center">
+                <h5>Liens Rapides</h5>
+                <ul class="list-unstyled">
+                    <li><a href="animals.php" class="text-white-50 text-decoration-none">Nos Animaux</a></li>
+                    <li><a href="visits.php" class="text-white-50 text-decoration-none">Réserver une Visite</a></li>
+                    <li><a href="asaad_lion.php" class="text-white-50 text-decoration-none">Lion de l'Atlas</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 mb-4 text-end">
+                <h5>Suivez-nous</h5>
+                <div class="social-icons">
+                    <a href="#" class="text-white me-3"><i class="fab fa-facebook fa-2x"></i></a>
+                    <a href="#" class="text-white me-3"><i class="fab fa-instagram fa-2x"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-twitter fa-2x"></i></a>
+                </div>
+                <p class="mt-3 text-white-50 small">© 2025 Zoo ASSAD - Spécial CAN Maroc</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/js/script.js"></script>
+</body>
+</html>
