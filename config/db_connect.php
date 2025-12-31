e4d82a09d3b4649a062ad5d8fb6f04ab7301e61c
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2025-12-31 22:11:39 +0100

    feat: création du fichier de configuration globale

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,6 @@
 <?php
-require_once '../Classes/DbConnection.php';
+include '../Classes/DbConnection.php';
+
 
 // $conn = new DbConnection("localhost","zoo_assad_v2","momo","momo1");
 $conn = new DbConnection("localhost","momo","momo1","zoo_assad_v2");
